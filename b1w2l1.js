151bef825902416d7c8994e8271d488df5f4b821
Dave <daveleeggangers123@hotmail.com>
2019-09-09 13:39:14 +0200

    Week 2 JavaScript Lab1

---
+++ b1w2l1.js
@@ -0,0 +1,10 @@
+/*Dave Leeggangers*/
+/*media en applicatie ontwikkeling*/
+
+var naam = prompt("voer je naam in", "Dave"); //dit maakt een variable voor een naam pop-up
+var achternaam = prompt("voer je achternaam in", "Leeggangers") //dit maakt een variable voor een naam pop-up
+var leeftijd = prompt("voer je leeftijd in", "17") //dit maakt een variable voor een naam pop-up
+
+  document.write(naam); //dit schrijft het ingevoerde antwoord op de pagina.
+  document.write(achternaam); //dit schrijft het ingevoerde antwoord op de pagina.
+  document.write(leeftijd); //dit schrijft het ingevoerde antwoord op de pagina.
